15b9c4bff0635af9cb634fecdddf23ea30a2efe0
Ayroid <ayushsk0000@gmail.com>
2023-02-09 21:36:54 +0530

    Snake Game ready for 1 Player

--- snake.sh
+++ snake.sh
@@ -1,48 +1,96 @@
 #!/bin/bash
 
-snakein=(18 26 39 51 56 75 83 90 92 97)
-snakeout=(1 10 5 6 19 28 45 48 25 66)
-ladderin=(3 7 11 15 17 38 49 57 73 81)
-ladderout=(20 13 28 34 74 59 67 76 96 98)
-
-player1=1
-player2=1
-
-play=0
-
-while [ $player1 != 100 -a $player2 != 100 ] 
+player=1
+counter=0
+echo 
+while [ $player != 100 ]
 do
-	play=$((play+1))
-	echo "Player$play:"
-	play=$((play%2))
-
-	read
-
-	number=0
-	while [ $number -lt 1 ]
-	do
-        	number=$(($RANDOM%7))
-	done
-
-	echo "You rolled $number"
+    read -p "Press Enter to throw Dice:"
+    dice=0
+    while [ $dice -lt 1 ]
+    do
+        dice=$((RANDOM%7))
+    done
+    
+    echo "You Rolled a $dice"
+    counter=$((counter+1))
+    
+    player=$((player+dice))
+    
+    if [ $player -gt 100 ]; then
+        player=$((player-dice))
+    fi
+    
+    case $player in
+        18)
+            player=1
+        echo "You got eaten by a Snake!";;
+        26)
+            player=10
+        echo "You got eaten by a Snake!";;
+        39)
+            player=5
+        echo "You got eaten by a Snake!";;
+        51)
+            player=6
+        echo "You got eaten by a Snake!";;
+        56)
+            player=19
+        echo "You got eaten by a Snake!";;
+        75)
+            player=28
+        echo "You got eaten by a Snake!";;
+        83)
+            player=45
+        echo "You got eaten by a Snake!";;
+        90)
+            player=48
+        echo "You got eaten by a Snake!";;
+        92)
+            player=25
+        echo "You got eaten by a Snake!";;
+        97)
+            player=66
+        echo "You got eaten by a Snake!";;
+        3)
+            player=20
+        echo "You climed a Ladder!";;
+        7)
+            player=13
+        echo "You climed a Ladder!";;
+        11)
+            player=28
+        echo "You climed a Ladder!";;
+        15)
+            player=34
+        echo "You climed a Ladder!";;
+        17)
+            player=74
+        echo "You climed a Ladder!";;
+        38)
+            player=59
+        echo "You climed a Ladder!";;
+        49)
+            player=67
+        echo "You climed a Ladder!";;
+        57)
+            player=76
+        echo "You climed a Ladder!";;
+        73)
+            player=96
+        echo "You climed a Ladder!";;
+        81)
+            player=98
+        echo "You climed a Ladder!";;
+    esac
+    
+    echo "SCORE: $player"
 	echo
-
-	if [ $play -eq 1 ]; then 
-		player1=$((player1 + number))
-
-		if [ $player1 -gt 100 ]; then
-          	      player1=$((player1-$number))
-        	fi
-
-	else
-		player2=$((player2 + number))
-
-		if [ $player2 -gt 100 ]; then
-          	      player2=$((player2-$number))
-       		fi
-	fi
-
-	echo "Score: P1 - $player1  P2 - $player2"
 done
 
-echo "Game Over!"
+echo "Number of times dice was rolled: $counter"
+
+
+
+
+
